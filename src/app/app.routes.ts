883dae4dbbe076de11765651348cc43794bab66d
pbeer <pawelbeer@gmail.com>
2025-05-25 17:05:51 +0200

    Dodawanie fiszek

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,12 @@ import {
   WordBankCategoriesAddComponent
 } from './word-bank/word-bank-categories/word-bank-categories-add/word-bank-categories-add.component';
 import {WordBankCardsComponent} from './word-bank/word-bank-cards/word-bank-cards.component';
+import {WordBankCardsAddComponent} from './word-bank/word-bank-cards/word-bank-cards-add/word-bank-cards-add.component';
 
 export const routes: Routes = [
   {path: '',component:HomeComponent},
   {path: 'word-bank/categories',component: WordBankCategoriesComponent},
   {path: 'word-bank/categories/add',component: WordBankCategoriesAddComponent},
-  {path: 'word-bank/categories/:categoryId/cards',component: WordBankCardsComponent}
+  {path: 'word-bank/categories/:categoryId/cards',component: WordBankCardsComponent},
+  {path: 'word-bank/categories/:categoryId/cards/add', component: WordBankCardsAddComponent}
 ];
