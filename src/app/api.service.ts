7360541988865f002df74bdcb3581806b7807989
pbeer <pawelbeer@gmail.com>
2025-05-25 15:16:57 +0200

    API Service - fiszki

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,7 +1,7 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {Category} from './word-bank/models';
+import {Card, Category} from './word-bank/models';
 
 const BASE_URL = 'http://localhost:5000';
 
@@ -10,6 +10,7 @@ const BASE_URL = 'http://localhost:5000';
 })
 export class ApiService {
   private readonly httpClient = inject(HttpClient);
+
 
   getCategories(): Observable<Category[]> {
     return this.httpClient.get<Category[]>(`${BASE_URL}/categories`);
@@ -23,5 +24,20 @@ export class ApiService {
     return this.httpClient.post<Category>(`${BASE_URL}/categories`, {
       category_name: categoryName
     })
+  }
+
+  getCards(categoryId: number): Observable<Card[]> {
+    return this.httpClient.get<Card[]>(`${BASE_URL}/categories/${categoryId}/cards`)
+  }
+
+  addCard(categoryId: number, word: string, definition:string): Observable<Card>{
+    return this.httpClient.post<Card>(`${BASE_URL}/categories/${categoryId}/cards`,{
+      word,
+      definition
+    })
+  }
+
+  removeCard(categoryId: number, cardId: number): Observable<any>{
+    return this.httpClient.delete(`${BASE_URL}/categories/${categoryId}/cards/${cardId}`)
   }
 }
